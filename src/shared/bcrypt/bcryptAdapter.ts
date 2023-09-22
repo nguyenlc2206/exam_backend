5fcd0ba5e803edf72d771f22e276ff1e107628af
nguyencaole <nguyenlc.smile@gmail.com>
2023-09-22 19:54:30 +0700

    update

--- src/shared/bcrypt/bcryptAdapter.ts
+++ src/shared/bcrypt/bcryptAdapter.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import { HashComparer, Hasher } from '../entities/hash.entity';
 
 export class BcryptAdapter implements Hasher, HashComparer {
