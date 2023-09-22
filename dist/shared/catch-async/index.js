8016cdc8b80575f1fa133e7c6dba6c87114201e9
nguyencaole <nguyenlc.smile@gmail.com>
2023-09-22 20:57:12 +0700

    update

--- dist/shared/catch-async/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/** Define try catch async */
-const catchAsync = (fn) => {
-    return (req, res, next) => {
-        fn(req, res, next).catch(next);
-    };
-};
-exports.default = catchAsync;
